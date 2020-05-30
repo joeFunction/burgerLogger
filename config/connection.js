ea2f6c3653a1ebca3f2e129ae9bea4dfd35b9f5d
Joe Adams <adams.jd10@gmail.com>
2020-05-29 20:50:32 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 const mysql = require("mysql");
 const util = require("util");
 
+if(process.env.JAWSDB_URL) {
+    connection =  mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
